560cdaea15b0f468c2cfba82bb1f0406fe586cc8
Andrew Kononenko <wastardy.k@gmail.com>
2024-10-29 20:23:30 +0200

    add functionality to 'new game' button

--- 3_pig_game/script.js
+++ 3_pig_game/script.js
@@ -4,7 +4,7 @@ let currentScore = 0;
 let activePlayer = 0;
 const scores = [0, 0];
 
-let playing = true;
+let isPlaying = true;
 
 // Selection elements player--active
 const player0 = document.querySelector('.player--0');
@@ -13,6 +13,8 @@ const scorePlayer0 = document.querySelector('#score--0');
 const scorePlayer1 = document.getElementById('score--1');
 const currentScorePlayer0 = document.getElementById('current--0');
 const currentScorePlayer1 = document.getElementById('current--1');
+
+const classNameToCheck = 'player--winner';
 
 const diceElement = document.querySelector('.dice');
 
@@ -37,7 +39,7 @@ const switchPlayer = () => {
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function() {
-    if (playing) {
+    if (isPlaying) {
         const dice = Math.trunc(Math.random() * 6) + 1;
 
         diceElement.classList.remove('hidden');
@@ -54,7 +56,7 @@ btnRoll.addEventListener('click', function() {
 });
 
 btnHold.addEventListener('click', function() {
-    if (playing) {
+    if (isPlaying) {
         scores[activePlayer] += currentScore;
         // scores[1] = scores[1] + currnetScore;
         
@@ -63,7 +65,7 @@ btnHold.addEventListener('click', function() {
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
         if (scores[activePlayer] >= 20) {
-            playing = false;
+            isPlaying = false;
 
             diceElement.classList.add('hidden');
             
@@ -79,4 +81,28 @@ btnHold.addEventListener('click', function() {
             switchPlayer();
         }
     }
+});
+
+btnNew.addEventListener('click', function() {
+    scorePlayer0.textContent = 0;
+    scorePlayer1.textContent = 0;
+    diceElement.classList.remove('hidden');
+
+    currentScore = 0;
+    currentScorePlayer0.textContent = 0;
+    currentScorePlayer1.textContent = 0;
+    scores[0, 0] = [];
+
+    if (player0.classList.contains(classNameToCheck)) {
+        player0.classList.remove('player--winner');
+        player1.classList.add('player--active');
+        activePlayer = 1;
+    }
+    else if (player1.classList.contains(classNameToCheck)) {
+        player1.classList.remove('player--winner');
+        player0.classList.add('player--active');
+        activePlayer = 0;
+    }
+
+    isPlaying = true;
 });
