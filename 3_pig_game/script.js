f70c576380c88cd72801fd575cecc6112bef3372
Andrew Kononenko <wastardy.k@gmail.com>
2024-10-29 17:01:15 +0200

    set score as 0 at the start & hide dice picture

--- 3_pig_game/script.js
+++ 3_pig_game/script.js
@@ -1,2 +1,10 @@
 'use strict';
 
+// Selection elements
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
